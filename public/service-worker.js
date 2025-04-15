f2c2c9ae5b0361d94e9db62b5f6455c8ea1167d1
zytaai <byeongju918@daum.net>
2025-04-15 15:53:44 +0900

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,12 @@
 
+self.addEventListener('install', function (event) {
+  console.log('[ServiceWorker] 설치 완료');
+});
+
+self.addEventListener('activate', function (event) {
+  console.log('[ServiceWorker] 활성화 완료');
+});
+
+self.addEventListener('fetch', function (event) {
+  // 기본 패스스루
+});
